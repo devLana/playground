e3486a6338844d8dd6687ea918b8f41cc7bffe73
ade_lana <lanadvictor@yahoo.co.uk>
2021-12-11 22:15:38 +0100

    ...

--- pagination/functions/content.js
+++ pagination/functions/content.js
@@ -2,7 +2,7 @@ const content = (length, page, itemsPerPage) => {
   const contents = document.querySelector(".contents");
 
   const data = new Array(length);
-  const dataKeys = [...data.keys()];
+  const dataKeys = Array.from(data.keys());
 
   const lastIndex = page * itemsPerPage;
   const firstIndex = lastIndex - itemsPerPage;
@@ -12,7 +12,7 @@ const content = (length, page, itemsPerPage) => {
   dataKeys.slice(firstIndex, lastIndex).forEach(data => {
     html += `
       <div class="content__item">
-        <p class="content">Item ${data + 1}</p>
+        <p class="content">Record ${data + 1}</p>
       </div>
     `;
   });
--- pagination/index.js
+++ pagination/index.js
@@ -2,7 +2,7 @@ import pagination from "./functions/pagination.js";
 import content from "./functions/content.js";
 
 const paginate = (page = 1) => {
-  const length = 150;
+  const length = 220;
   const itemsPerPage = 8;
 
   content(length, page, itemsPerPage);
